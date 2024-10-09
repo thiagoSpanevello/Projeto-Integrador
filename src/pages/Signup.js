c9818b897d970d327cf2210b359041da508ae37d
Thiago <thspanevello@gmail.com>
2024-10-09 18:23:42 -0300

    signup view ready

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react'
 import "../styles/Signup.css"
 function Signup() {
     const [CNPJ, setCNPJ] = useState("");
-    const [nome, setNome] = useState("admin@");
+    const [conta, setConta] = useState("admin@");
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [message, setMessage] = useState('');
     function maskCNPJ(value) {
         value = value.replace(/\D/g, ''); // Remove tudo que não for número
         // Máscara de CNPJ (00.000.000/0000-00)
@@ -18,8 +21,30 @@ function Signup() {
         let text = valor.split(' ')[0];
         let a = "admin@";
         text = a + text;
-        setNome(text);
+        setConta(text);
     }
+
+    const validatePassword = (pass, confirmPass) => {
+        if (!pass || !confirmPass) {
+            setMessage('');
+        } else if (pass === confirmPass) {
+            setMessage('');
+        } else {
+            setMessage("Senhas não correspondem");
+        }
+    }
+
+    const handlePasswordChange = (e) => {
+        const newPassword = e.target.value
+        setPassword(newPassword);
+        validatePassword(newPassword, confirmPassword);
+    }
+    const handleConfirmPasswordChange = (e) => {
+        const newConfirmPassword = e.target.value
+        setConfirmPassword(newConfirmPassword);
+        validatePassword(password, newConfirmPassword);
+    }
+
 
 
     return (
@@ -37,11 +62,12 @@ function Signup() {
                         <label htmlFor='CNPJ'>Nome da empresa</label>
                         <input type='text' id='Name' autoComplete='off' placeholder='nome da empresa' required onChange={handleChange} />
                         <label htmlFor='Conta'>Conta</label>
-                        <input type='text' id='Conta' autoComplete='off' disabled value={nome} />
+                        <input type='text' id='Conta' autoComplete='off' disabled value={conta} />
                         <label htmlFor='Password'>Senha</label>
-                        <input type='password' id='Password' autoComplete='off' placeholder='enter your password' required />
+                        <input type='password' value={password} onChange={handlePasswordChange} id='Password' autoComplete='off' placeholder='enter your password' required />
                         <label htmlFor='Password'>Confirmar senha</label>
-                        <input type='password' id='ConfirmPassword' autoComplete='off' placeholder='enter your password' required />
+                        <input type='password' id='ConfirmPassword' value={confirmPassword} onChange={handleConfirmPasswordChange} autoComplete='off' placeholder='enter your password' required />
+                        {message && <p className='passwordMessage'>{message}</p>}
                         <button type="submit" class="btn btn-primary">Save</button>
                     </div>
                 </form>
