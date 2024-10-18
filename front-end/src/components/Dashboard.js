871e8b4c3a796dd04e350b3ffc27fcd21c6c8678
Vitor De Souza Bach <vitibach@gmail.com>
2024-10-18 19:30:01 -0300

    protótipo sidebar e dashboard

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React from 'react';
+import './Dashboard.css';
 
-function Dashboard() {
-    return <div>
-        <p>Aqui sera a dashboard</p>
-    </div>
-}
+const Dashboard = () => {
+    const ganhos = [1200, 1500, 800, 2000, 1700, 1900, 2200, 2500, 3000, 2700, 3100, 4000];
+    const meses = [
+        'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 
+        'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 
+        'Novembro', 'Dezembro'
+    ];
+
+    return (
+        <div className="dashboard">
+            <h1>Ganhos Mensais</h1>
+            <div className="grafico">
+                {ganhos.map((ganho, index) => (
+                    <div key={index} className="barra" style={{ height: `${ganho / 40}px` }}>
+                        <span className="valor">R$ {ganho}</span>
+                    </div>
+                ))}
+            </div>
+            <div className="meses">
+                {meses.map((mes, index) => (
+                    <span key={index} className="mes">{mes}</span>
+                ))}
+            </div>
+        </div>
+    );
+};
+
 export default Dashboard;
