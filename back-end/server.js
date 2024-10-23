223fc9e63ddef8152990422e7e998b7bb2a73be9
Vitor De Souza Bach <vitibach@gmail.com>
2024-10-22 21:38:04 -0300

    Continuação de rotas

--- back-end/server.js
+++ back-end/server.js
@@ -7,19 +7,17 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.listen(3001, () => console.log("Rodando porta 3001"));
 
+
 // Rotas de Usuário
 app.post('/cadastro/admin', (req, res) => {
-    console.log("entrou na rota de cadastro de admin");
     const CNPJ = req.body.CNPJ;
     const nome = req.body.nome;
     const conta = req.body.conta;
     const senha = req.body.senha;
     res.send({ "CNPJ": CNPJ, "nome": nome, "conta": conta });
-
 });
 
 app.post('/cadastro/funcionarios', (req, res) => {
-    console.log("entrou na rota de cadastro de funcionários");
     const nome = req.body.nome;
     const CPF = req.body.CPF;
     const conta = req.body.conta;
@@ -29,9 +27,58 @@ app.post('/cadastro/funcionarios', (req, res) => {
 });
 
 app.get('/relatorio/funcionarios', (req, res) => {
-    console.log("entrou na rota de listagem de funcionários");
-    res.send("teste pra saber se retornou certinho");
+    res.send("entrou na rota de listagem de funcionários");
 })
 
 
+//Rotas de Tipos de Serviços
 
+app.post('/cadastro/tiposervicos', (req, res) => {
+    const nome = req.body.nome;
+    const descricao = req.body.descricao;
+    res.send({ "Descrição": descricao, "nome": nome,});
+});
+
+app.get('/relatorio/tiposervico', (req, res) => {
+    res.send("entrou na rota de listagem de tipos de serviços");
+})
+
+
+//Rotas de Serviços
+
+app.post('/cadastro/servicos', (req, res) => {
+    const nome = req.body.nome;
+    const CNPJ = req.body.CNPJ;
+    const data = new Date();
+    console.log(data);
+    res.send({ "Nome": nome, "CNPJ": CNPJ,"Data": data});
+});
+
+app.get('/relatorio/servico', (req, res) => {
+    res.send("entrou na rota de listagem de serviços");
+})
+
+
+//Rotas de Pagamentos
+
+app.post('/cadastro/pagamentos', (req, res) => {
+    const valor = req.body.valor;
+    const data = new Date();
+    console.log(data);
+    res.send({ "Valor": valor,"Data": data});
+});
+
+app.get('/relatorio/pagamentos', (req, res) => {
+    res.send("Relatório dos pagamentos cadastrados que ainda nao foram encerrados");
+})
+
+
+//Rotas de Dashboards
+
+app.get('/relatorio/dashboard', (req, res) => {
+    res.send(" Valor liquido mensal geral de 6 meses da empresa");
+})
+
+app.get('/relatorio/RedirecionaFuncionario', (req, res) => {
+    res.send("Redirecionado diretamente para o relatório de pagamentos");
+})
