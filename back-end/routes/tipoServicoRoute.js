091a39d470ce0d8126b825103e868b2265f4bec6
Thiago <thspanevello@gmail.com>
2024-12-04 16:43:23 -0300

    todas as rotas de tiposervico

--- back-end/routes/tipoServicoRoute.js
+++ back-end/routes/tipoServicoRoute.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post('/cadastro/tipoServico', verifyPermissions(["empresa", "gerente"]), addTipoServico);
 router.get('/listagem/tipoServico', listTipoServicos);
+router.put('/update/tipoServico/:id', verifyPermissions(["empresa", "gerente"]), updateTipoServico);
+router.delete('/excluir/tipoServico/:id', verifyPermissions(["empresa", "gerente"]), deleteTipoServico);
 export default router;
